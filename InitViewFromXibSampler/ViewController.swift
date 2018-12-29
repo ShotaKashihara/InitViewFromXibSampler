3a45345715872abbda819ff12cf399a3dd5b2b55
Shota Kashihara <shota.kashihara.1004@gmail.com>
2018-12-29 19:38:41 +0900

    Initial Commit

---
+++ InitViewFromXibSampler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InitViewFromXibSampler
+//
+//  Created by wantedly on 2018/12/29.
+//  Copyright © 2018 com.shota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
